07c6fe005320b6ef08b89d62ee68cdb1ae97a0c0
Amritansh Tripathi <tripathi.a@husky.neu.edu>
2018-06-02 17:35:21 -0400

    updated project

--- src/main/java/edu/northeastern/cs5200/Cs5200Spring2018TripathiApplication.java
+++ src/main/java/edu/northeastern/cs5200/Cs5200Spring2018TripathiApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Cs5200Spring2018TripathiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Cs5200Spring2018TripathiApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs5200Spring2018TripathiApplication.class, args);
+    }
 }
--- src/main/java/edu/northeastern/cs5200/controller/hello/HelloController.java
+++ src/main/java/edu/northeastern/cs5200/controller/hello/HelloController.java
@@ -1,11 +1,51 @@
 package edu.northeastern.cs5200.controller.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 public class HelloController {
+
+    @Autowired
+
+    HelloRepository helloRepository;
+
+    @RequestMapping("/api/hello/insert")
+
+    public HelloObject insertHelloObject() {
+
+        HelloObject obj = new HelloObject("Hello Amritansh Tripathi!");
+
+        helloRepository.save(obj);
+
+        return obj;
+
+    }
+
+    @RequestMapping("/api/hello/select/all")
+    public List<HelloObject> selectAllHelloObjects() {
+
+        List<HelloObject> hellos = (List<HelloObject>) helloRepository.findAll();
+
+        return hellos;
+
+    }
+
+    @RequestMapping("/api/hello/insert/{msg}")
+    public HelloObject insertMessage(@PathVariable("msg") String message) {
+
+        HelloObject obj = new HelloObject(message);
+
+        helloRepository.save(obj);
+
+        return obj;
+
+    }
 
     @RequestMapping("/api/hello/string")
     public String sayHello() {
